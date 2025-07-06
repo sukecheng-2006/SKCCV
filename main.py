03bdfae688b806be3cf42fac089efd48a2192cf3
“sukecheng-2006” <1447100593@qq.com>
2025-07-06 16:18:04 +0800

    梯度

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import torch
-x=torch.ones(2,2,requires_grad=True)
-out=x.sum()
-out.backward()
+x=torch.tensor([1.0,2.0,3.0,4.0],requires_grad=True)
+y=2*x
+z=y.view(2,2)
+v=torch.tensor([[1.0,0.1],[0.01,0.001]],dtype=torch.float)
+l=torch.sum(z*v)
+#print(l)
+z.backward(v)
+print(x.grad)
+
