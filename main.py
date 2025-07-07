44990fc4c6c35ddf08198e98f82ab179ac25447f
“sukecheng-2006” <1447100593@qq.com>
2025-07-07 12:28:48 +0800

    softmax

--- main.py
+++ main.py
@@ -21,4 +21,18 @@
 # #     c[i]=a[i]+b[i]
 # d=a+b
 # print(time()-start)
-%matplotlib inline
+# import matplotlib_inline
+# import torch
+# from IPython import display
+# from matplotlib import pyplot as plt
+# import numpy as np
+# import random
+# num_inputs=2
+# num_examples=1000
+# true_w=[2,-3.4]
+# true_b=4.2
+# features=torch.from_numpy(np.random.normal(0,1,(num_examples,num_inputs)))
+# labels=true_w[0]*features[:,0]+true_w[1]*features[:,1]+true_b
+# labels+=torch.from_numpy(np.random.normal(0,0.01,size=labels.size()))
+
+# print(labels)
